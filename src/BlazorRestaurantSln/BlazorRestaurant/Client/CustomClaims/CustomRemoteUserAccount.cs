888774f6ef84ba5f1c3c358f7eb8101ec9b720c6
Eduardo Fonseca <eduardo.fonseca@pticostarica.com>
2021-04-24 13:47:55 -0600

    Persisting users log in data

---
+++ src/BlazorRestaurantSln/BlazorRestaurant/Client/CustomClaims/CustomRemoteUserAccount.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorRestaurant.Client.CustomClaims
+{
+    public class CustomRemoteUserAccount: RemoteUserAccount
+    {
+    }
+}
--- src/BlazorRestaurantSln/BlazorRestaurant/Server/Program.cs
+++ src/BlazorRestaurantSln/BlazorRestaurant/Server/Program.cs
@@ -9,13 +9,25 @@ using System.Threading.Tasks;
 
 namespace BlazorRestaurant.Server
 {
+    /// <summary>
+    /// Application Entry class
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Application Entry point
+        /// </summary>
+        /// <param name="args"></param>
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
 
+        /// <summary>
+        /// Application Initializer
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
